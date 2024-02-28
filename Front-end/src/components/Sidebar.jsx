895642a06765f168dda252db0d3431cf4e256e86
Danuh16 <danuhishe3708@gmail.com>
2024-02-28 09:13:31 +0300

    front-end

---
+++ Front-end/src/components/Sidebar.jsx
@@ -0,0 +1,228 @@
+import React from "react";
+import {
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import {
+  ChevronLeft,
+  ChevronRightOutlined,
+  HomeOutlined,
+  WorkOutlined,
+  DescriptionOutlined,
+  CheckBoxOutlined,
+  CalendarMonthOutlined,
+  PeopleOutlined,
+  PlaceOutlined,
+  FolderOpenOutlined,
+  MeetingOutlined
+} from "@mui/icons-material";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import FlexBetween from "./FlexBetween";
+
+
+
+
+
+
+
+const navItems = [
+  {
+    text: "Main",
+    icon: null,
+  },
+  {
+    text: "Home",
+    icon: <HomeOutlined />,
+  },
+  {
+    text: "Jobs",
+    icon: <WorkOutlined />,
+  },
+  {
+    text: "Resumes",
+    icon: <DescriptionOutlined />,
+  },
+  {
+    text: "Tasks",
+    icon: <CheckBoxOutlined />,
+  },
+  {
+    text: "Calender",
+    icon: <CalendarMonthOutlined />,
+  },
+  {
+    text: "Admin",
+    icon: null,
+  },
+  {
+    text: "Users",
+    icon: <PeopleOutlined />,
+  },
+  {
+    text: "Locations",
+    icon: <PlaceOutlined />,
+  },
+  {
+    text: "Job Boards",
+    icon: <FolderOpenOutlined />,
+  },
+  {
+    text: "Categories",
+    icon: null,
+  },
+  {
+    text: "Business",
+    icon: null,
+  },
+  {
+    text: "Working",
+    icon:null ,
+  },
+  {
+    text: "Management",
+    icon: null,
+  },
+];
+
+
+
+const Sidebar = ({
+  user,
+  drawerWidth,
+  isSidebarOpen,
+  setIsSidebarOpen,
+  isNonMobile,
+}) => {
+  const { pathname } = useLocation();
+  const [active, setActive] = useState("");
+  const navigate = useNavigate();
+  const theme = useTheme();
+
+  useEffect(() => {
+    setActive(pathname.substring(1));
+  }, [pathname]);
+
+  return (
+    <Box component="nav">
+      {isSidebarOpen && (
+        <Drawer
+          open={isSidebarOpen}
+          onClose={() => setIsSidebarOpen(false)}
+          variant="persistent"
+          anchor="left"
+          sx={{
+            width: drawerWidth,
+            "& .MuiDrawer-paper": {
+              backgroundColor: "blue",
+              boxSixing: "border-box",
+              borderWidth: isNonMobile ? 0 : "2px",
+              width: drawerWidth,
+            },
+          }}
+        >
+          <Box width="100%">
+            <Box m="1.5rem 2rem 2rem 3rem">
+              <FlexBetween color={"white"}>
+                <Box display="flex" alignItems="center" gap="0.5rem">
+                  <Box
+                    icon="MeetingOutlined"
+                  />
+                  <Typography variant="h4" fontWeight="bold">
+                    Meets
+                  </Typography>
+                </Box>
+                {!isNonMobile && (
+                  <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+                    <ChevronLeft />
+                  </IconButton>
+                )}
+              </FlexBetween>
+            </Box>
+            <List>
+              {navItems.map(({ text, icon }) => {
+                if (!icon) {
+                  return (
+                    <Typography key={text} sx={{ m: "2.25rem 0 1rem 3rem" }}>
+                      {text}
+                    </Typography>
+                  );
+                }
+                const lcText = text.toLowerCase();
+
+                return (
+                  <ListItem key={text} disablePadding>
+                    <ListItemButton
+                      onClick={() => {
+                        navigate(`/${lcText}`);
+                        setActive(lcText);
+                      }}
+                      sx={{
+                        backgroundColor:
+                          active === lcText
+                            ? theme.palette.secondary[500]
+                            : "transparent",
+                        color:
+                          active === lcText
+                            ? theme.palette.primary[600]
+                            : theme.palette.secondary[100],
+                      }}
+                    >
+                      <ListItemIcon
+                        sx={{
+                          ml: "2rem",
+                          color:
+                            active === lcText
+                              ? theme.palette.primary[600]
+                              : theme.palette.secondary[200],
+                        }}
+                      >
+                        {icon}
+                      </ListItemIcon>
+                      <ListItemText primary={text} />
+                      {active === lcText && (
+                        <ChevronRightOutlined sx={{ ml: "auto" }} />
+                      )}
+                    </ListItemButton>
+                  </ListItem>
+                );
+              })}
+            </List>
+          </Box>
+
+          <Box position="absolute" bottom="2rem">
+            <Divider />
+            <FlexBetween textTransform="none" gap="1rem" m="1.5rem 2rem 0 3rem">
+              <Box textAlign="left">
+                <Typography
+                  fontWeight="bold"
+                  fontSize="0.9rem"
+                  sx={{ color: theme.palette.secondary[100] }}
+                >
+                  {user.name}
+                </Typography>
+                <Typography
+                  fontSize="0.8rem"
+                  sx={{ color: theme.palette.secondary[200] }}
+                >
+                  {user.occupation}
+                </Typography>
+              </Box>
+            </FlexBetween>
+          </Box>
+        </Drawer>
+      )}
+    </Box>
+  );
+};
+
+export default Sidebar;
